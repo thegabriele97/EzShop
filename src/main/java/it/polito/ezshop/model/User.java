4c2aeb642a43309b8cb1f9922eb85d454335fa4e
Salvatore Gabriele La Greca <gabriele.lagreca@gmail.com>
2021-05-05 12:56:35 +0200

    feat: added TODO in class User: to be implemented

--- src/main/java/it/polito/ezshop/model/User.java
+++ src/main/java/it/polito/ezshop/model/User.java
@@ -1,7 +1,20 @@
 package it.polito.ezshop.model;
 //Simone
 import java.io.Serializable;
+import it.polito.ezshop.data.*;
 
-public class User implements Serializable {
+public class User implements Serializable, it.polito.ezshop.data.User {
     
+    @Override
+    public int hashCode() {
+        // TODO: return directly the user's ID
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO: return true if users'ID are equals, false otherwise
+        return super.equals(obj);
+    }
+
 }

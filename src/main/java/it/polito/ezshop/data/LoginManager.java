19f1dcdd8a66dc17f8f6b93c9ad021b3f227359d
Salvatore Gabriele La Greca <gabriele.lagreca@gmail.com>
2021-05-05 12:56:49 +0200

    feat: added LoginManager class

---
+++ src/main/java/it/polito/ezshop/data/LoginManager.java
@@ -0,0 +1,58 @@
+package it.polito.ezshop.data;
+import static java.util.stream.Collectors.*;
+import java.util.List;
+
+class LoginManager {
+    
+    private static LoginManager instance;
+
+    private it.polito.ezshop.model.User loggedUser;
+
+    private LoginManager() {
+        loggedUser = null;
+    }
+
+    static LoginManager getInstance() {
+        
+        if (instance == null) {
+            instance = new LoginManager();
+        }
+
+        return instance;
+    }
+
+    boolean isUserLogged() {
+        return loggedUser != null;
+    }
+
+    boolean isUserLogged(it.polito.ezshop.model.User user) {
+        return isUserLogged() && loggedUser.equals(user);
+    }
+
+    it.polito.ezshop.model.User getLoggedUser() {
+        return loggedUser;
+    }
+
+    boolean tryLogin(String username, String password) {
+        
+        List<it.polito.ezshop.model.User> requestedUser = DataManager.getInstance()
+            .getUsers()
+            .stream()
+            .filter(user -> user.getUsername().equals(username) && user.getPassword().equals(password))
+            .collect(toList());
+
+        if (requestedUser.size() != 1) return false;
+
+        loggedUser = requestedUser.get(0);
+        return true;
+    }
+    
+    boolean tryLogout() {
+
+        if (loggedUser == null) return false;
+
+        loggedUser = null;
+        return true;
+    }
+
+}

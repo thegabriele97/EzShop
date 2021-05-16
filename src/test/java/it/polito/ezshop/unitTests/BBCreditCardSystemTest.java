aff6d60cf27df4a5e6e95078907ec9c1659738f2
Alessandro Baldazzi <s289555@studenti.polito.it>
2021-05-16 17:31:10 +0200

    added some creditcard tests and minor fixes

--- src/test/java/it/polito/ezshop/unitTests/BBCreditCardSystemTest.java
+++ src/test/java/it/polito/ezshop/unitTests/BBCreditCardSystemTest.java
@@ -8,6 +8,8 @@ import org.junit.*;
 
 public class BBCreditCardSystemTest {
     
+    //isValidNumber()
+
     @Test
     public void testValidCreditCardWithCharacters() {
         assertFalse(CreditCardSystem.getInstance().isValidNumber("123s23xa223"));
@@ -38,6 +40,8 @@ public class BBCreditCardSystemTest {
         assertFalse(CreditCardSystem.getInstance().isValidNumber(null));
     }
 
+    //isRegistered()
+
     @Test
     public void testCardRegisteredWValidCreditCard() {
         assertTrue(CreditCardSystem.getInstance().isRegistered("9254347527611304"));
@@ -52,5 +56,72 @@ public class BBCreditCardSystemTest {
     public void testCardRegisteredWNoValidCreditCard() {
         assertFalse(CreditCardSystem.getInstance().isRegistered("999999999999"));
     }
+
+    @Test
+    public void testCardRegisteredWithCharacters() {
+        assertFalse(CreditCardSystem.getInstance().isRegistered("123s23xa223"));
+    }
+
+    @Test
+    public void testCardRegisteredWEmptyString() {
+        assertFalse(CreditCardSystem.getInstance().isRegistered(""));
+    }
+
+    @Test
+    public void testCardRegisteredWNullString() {
+        assertFalse(CreditCardSystem.getInstance().isRegistered(null));
+    }
+
+    //hasEnoughBalance()
+
+    @Test
+    public void testHasBalanceWValidCreditCard(){
+        assertTrue(CreditCardSystem.getInstance().hasEnoughBalance("9254347527611304", 10.0));
+    }
+
+    @Test
+    public void testHasBalanceWInvalidCreditCard(){
+        assertFalse(CreditCardSystem.getInstance().hasEnoughBalance("sas", 10.0));
+    }
+
+    @Test
+    public void testHasBalanceWUnregisteredCreditCard(){
+        assertFalse(CreditCardSystem.getInstance().hasEnoughBalance("123456789015", 10.0));
+    }
+
+    @Test
+    public void testHasBalanceWNotEnoughBalance(){
+        assertFalse(CreditCardSystem.getInstance().hasEnoughBalance("9254347527611304", 100.0));
+    }
+
+    @Test
+    public void testHasBalanceWNegativeToRemove(){
+        assertTrue(CreditCardSystem.getInstance().hasEnoughBalance("9254347527611304", -10.0)); 
+        //Ma è ok che sia true? (no controlli sul toRemove)
+    }
+
+    @Test
+    public void testHasBalanceWZeroToRemove(){
+        assertTrue(CreditCardSystem.getInstance().hasEnoughBalance("9254347527611304", 0.0));
+    }
+
+    @Test
+    public void testHasBalanceWNaNToRemove(){
+        assertFalse(CreditCardSystem.getInstance().hasEnoughBalance("9254347527611304", Double.NaN));
+    }
+
+    @Test
+    public void testHasBalanceWPositiveInfinityToRemove(){
+        assertFalse(CreditCardSystem.getInstance().hasEnoughBalance("9254347527611304", Double.POSITIVE_INFINITY));
+    }
+
+    @Test
+    public void testHasBalanceWNegativeInfinityToRemove(){
+        assertFalse(CreditCardSystem.getInstance().hasEnoughBalance("9254347527611304", Double.POSITIVE_INFINITY));
+    }
+
+    //updateBalance()
+
+    
 
 }

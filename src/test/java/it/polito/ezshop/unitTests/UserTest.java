e99ab5cddc26f194600a105ee053eed527a090c7
Francesco D'Anzi s290273 <s290273@studenti.polito.it>
2021-05-17 22:49:15 +0200

    fix: fixed UserTest

--- src/test/java/it/polito/ezshop/unitTests/UserTest.java
+++ src/test/java/it/polito/ezshop/unitTests/UserTest.java
@@ -8,53 +8,37 @@ import static org.junit.Assert.*;
 public class UserTest {
 
     @Test
-    public void testSetAndGetPassword() {
+    public void testSettersAndGettersOfUser() {
         User u = new User(1, "we", "mbare", "Administrator");
         assertEquals("mbare", u.getPassword());
-    }
-
-    @Test
-    public void testSetAndGetNullPassword() {
-        User u = new User(1, "we", null, "Administrator");
-        assertEquals(null, u.getPassword());
-    }
-
-
-    @Test
-    public void testSetAndGetUsername() {
-        User u = new User(1, "we", "mbare", "Administrator");
         assertEquals("we", u.getUsername());
-    }
-
-    @Test
-    public void testSetAndGetNullUsername() {
-        User u = new User(1, null, "mbare", "Administrator");
-        assertEquals(null, u.getUsername());
-    }
-
-    @Test
-    public void testSetAndGetId() {
-        User u = new User(1, "we", "mbare", "Administrator");
         assertEquals(new Integer(1), u.getId());
-    }
-
-    @Test
-    public void testSetAndGetInvalidId() {
-        User u = new User(1, "we", "mbare", "Administrator");
-        u.setId(-1);
-        assertEquals(new Integer(1), u.getId());
-    }
-
-    @Test
-    public void testSetAndGetRole() {
-        User u = new User(1, "we", "mbare", "Administrator");
         assertEquals("Administrator", u.getRole());
     }
 
     @Test
-    public void testSetAndGetInvalidRole() {
-        User u = new User(1, "we", "mbare", "Mbare");
-        assertEquals(null, u.getRole());
+    public void testSetNullPassword() {
+        User u = new User(1, "we", "mbare", "Administrator");
+        assertThrows(IllegalArgumentException.class, () -> { u.setPassword(null); });
+    }
+
+    @Test
+    public void testSetNullUsername() {
+        User u = new User(1, "we", "mbare", "Administrator");
+        assertThrows(IllegalArgumentException.class, () -> { u.setUsername(null); });
+    }
+
+    @Test
+    public void testSetInvalidId() {
+        User u = new User(1, "we", "mbare", "Administrator");
+        assertThrows(IllegalArgumentException.class, () -> { u.setId(-1); });
+
+    }
+
+    @Test
+    public void testSetInvalidRole() {
+        User u = new User(1, "we", "mbare", "Administrator");
+        assertThrows(IllegalArgumentException.class, () -> { u.setRole("mbare"); });
     }
 
     @Test
